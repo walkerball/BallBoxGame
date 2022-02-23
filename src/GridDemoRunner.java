d6f220e1a386ed23651f30d23d6710a41e3f42a4
colin-dsouza <colintdsouza@gmail.com>
2022-02-23 14:08:32 -0600

    test 2

--- src/GridDemoRunner.java
+++ src/GridDemoRunner.java
@@ -7,6 +7,7 @@ public class GridDemoRunner
 		// by default, we're using the StartHereFrame so you can get used to the Cell class;
 		// switch the commenting between these lines when you are ready for thaaaae full program.
 //		StartHereFrame practiceApp = new StartHereFrame();
+		//test2
 		GridDemoFrame app = new GridDemoFrame();
 
 	}
